9a956a407fb3752cf7021f3748fab0873750de50
alokkavilkar <120774363+alokkavilkar@users.noreply.github.com>
2024-07-28 13:30:16 +0530

    Update server-conf.py

--- learn/chapter9/server-conf.py
+++ learn/chapter9/server-conf.py
@@ -28,6 +28,47 @@ list_of_dicts = [
 # Print the list of dictionaries
 print(list_of_dicts)
 
+import json
+
+
+str = '{"name": "alok"}'
+dict = json.loads(str)
+print(dict)
+
+# dumps -> input = dictionary returns string
+# loads -> input string, retruns dict
+# dump works only with with open
+# load also
+
+
+
+python_dict = {'name': 'alok', 'age': 30}
+json_string = json.dumps(python_dict)
+dict = json.loads(json_string)
+print(json_string, dict)  
+
+
+list_of_dicts = [
+    {"name": "John", "age": 30},
+    {"name": "Alice", "age": 25},
+    {"name": "Bob", "age": 35}
+]
+
+with open('server-conf.json', 'w+') as f:
+    json.dump(list_of_dicts, fp=f, indent=4)
+
+
+with open('server-conf.json', 'w') as f:
+    f.write(json.dumps(list_of_dicts))
+
+str = '{"name": "alok"}'
+dict = json.loads(str)
+print(dict)
+
+with open('server-conf.json', 'r') as f:
+    doc = json.load(f)
+
+print(doc)
 
 with open('server-conf.json', 'w') as f:
     json.dump(list_of_dicts, f, indent=4)
