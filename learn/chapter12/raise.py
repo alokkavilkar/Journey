dff2bd3ec86530ea2af056052e5e2bde567748a2
alokkavilkar <120774363+alokkavilkar@users.noreply.github.com>
2024-08-04 13:04:50 +0530

    Update raise.py

--- learn/chapter12/raise.py
+++ learn/chapter12/raise.py
@@ -12,4 +12,30 @@ except Exception as e:
 
 # custom exception with class
 
-class 
+class MyException(Exception):
+    def __init__(self, password):
+        self.number = password
+        super().__init__('Enter character with less then 10 length')
+    
+    
+class SymbolException(Exception):
+    
+    def __init__(self, password):
+        self.password = password
+        super().__init__('Enter a symbol $ in password please..')
+        
+def login(password):
+    if len(password) > 10:
+        raise MyException(password)
+    if password.count('$') == 0:
+        raise SymbolException(password)
+    return "Login success...."
+
+try:
+    result = login('alokkav$')
+except MyException as e:
+    print(e)
+except SymbolException as s:
+    print(s)
+else:
+    print(result)
