74bae8fd344af0942cb2b2f08f43e58af2042dd8
alokkavilkar <120774363+alokkavilkar@users.noreply.github.com>
2024-08-12 11:21:59 +0530

    Update Map-sort.py

--- learn/chapter5/Map-sort.py
+++ learn/chapter5/Map-sort.py
@@ -14,11 +14,14 @@ dic = {
     'b':3,
 }
 
+def comparatornew(item1, item2):
+    # print(item1)
+    if item1[1] < item2[1]: return -1
+    elif item1[1] > item2[1] : return 1
+    else: return 0
+    
+# val = sorted(dic.keys(), key=cmp_to_key(comparator))
+# print({v : dic[v] for v in val})
 
-val = sorted(dic.keys(), key=cmp_to_key(comparator))
-dict1 = {v: dic[v] for v in val}
-print(dict1)
-
-dict1 = dict(sorted(dic.items(), key=cmp_to_key(comparator)))
-
-             
+val = dict(sorted(dic.items(), key=cmp_to_key(comparatornew)))
+print(val)
